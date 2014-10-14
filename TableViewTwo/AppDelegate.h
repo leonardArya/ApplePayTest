8981b8c44e0c6ec049e0e9d775e86d1c44f35696
lhc069 <lhc069@gmail.com>
2014-10-13 22:22:11 -0700

    Initial Commit

---
+++ TableViewTwo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TableViewTwo
+//
+//  Created by clark Lyu on 10/13/14.
+//  Copyright (c) 2014 clark Lyu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TableViewTwo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TableViewTwo
+//
+//  Created by clark Lyu on 10/13/14.
+//  Copyright (c) 2014 clark Lyu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
